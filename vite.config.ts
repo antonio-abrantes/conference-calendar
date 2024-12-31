d1234185d67440f0b49d68b491d85beaba8c3ce6
antonio-abrantes <antonio-abrantes@hotmail.com.br>
2024-12-31 15:18:59 -0300

    adjusted build app 01

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
@@ -9,7 +12,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
 });
